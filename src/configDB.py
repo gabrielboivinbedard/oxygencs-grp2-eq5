14897fb0c6cb302fc87b75ffbfc174bce5116717
Raphael Dupuis <raphael.dupuis.1@ens.etsmtl.ca>
2024-03-09 07:46:17 -0500

    Modification du path des tests et multiples corrections de lints

--- src/configDB.py
+++ src/configDB.py
@@ -1,28 +1,33 @@
 from configparser import ConfigParser
 import psycopg2
 
-def load_config(filename='./src/database.ini', section='postgresql'):
 
-    parser = ConfigParser()
-    parser.read(filename)
+class DatabaseConfig:
+    def __init__(self, filename="./src/database.ini", section="postgresql"):
+        self.filename = filename
+        self.section = section
 
-    # get section, default to postgresql
-    config = {}
-    if parser.has_section(section):
-        params = parser.items(section)
-        for param in params:
-            config[param[0]] = param[1]
-    else:
-        raise Exception('Section {0} not found in the {1} file'.format(section, filename))
+    def load_config(self):
+        parser = ConfigParser()
+        parser.read(self.filename)
+        config = {}
+        if parser.has_section(self.section):
+            params = parser.items(self.section)
+            for param in params:
+                config[param[0]] = param[1]
+        else:
+            raise Exception(
+                f"Section [{self.section}] not found in the [{self.filename}] file"
+            )
+        return config
 
-    return config
-
-def connect(config):
-    """ Connect to the PostgreSQL database server """
-    try:
-        # connecting to the PostgreSQL server
-        with psycopg2.connect(**config) as conn:
-            print('Connected to the PostgreSQL server.')
-            return conn
-    except (psycopg2.DatabaseError, Exception) as error:
-        print(error)
+    def connect(self, config):
+        try:
+            with psycopg2.connect(**config) as conn:
+                print("Connected to the PostgreSQL server.")
+                return conn
+        except (psycopg2.DatabaseError, Exception) as error:
+            print(error)
+            raise ConnectionError(
+                "Failed to connect to the PostgreSQL server."
+            ) from error
